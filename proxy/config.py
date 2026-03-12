06bc2b4ada7dfa0052cfb186ff17cd689862bd33
pflaumax <maksym.pflaum@altexsoft.com>
2026-03-12 10:06:07 +0200

    refactor: move headers to constants.py; add extra ignore config; add exception error for proxy handler to get clear detals

--- proxy/config.py
+++ proxy/config.py
@@ -4,7 +4,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     upstream_base_url: str = "https://httpbin.org"
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
 
 
 settings = Settings()
